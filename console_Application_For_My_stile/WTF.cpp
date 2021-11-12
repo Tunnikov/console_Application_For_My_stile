6b2fcc025a109f66c4b6fa951b133393f43f5830
tunnikovartur@gmail.com <tunnikovartur@gmail.com>
2021-11-12 06:15:04 +0300

    Убрал библиотеку stdlib.h (она не играла никакую роль) Вставил оператор break в проверку (наличие 6-ки в строке) Изменил в 3 и 4 циклах поменял буквы i и j Перенёс bool a в начало кода

--- console_Application_For_My_stile/WTF.cpp
+++ console_Application_For_My_stile/WTF.cpp
@@ -1,61 +1,4 @@
-/*/
 #include <iostream>
-#include <conio.h>
-#include <string>
-#include <string.h>
-
-using namespace std;
-//Определить является ли введенный текст: идентификатором, записью целого числа.
-
-struct name_Of_Indefekator
-{
-	char name[50];
-};
-
-
-int main()
-{
-	setlocale(LC_ALL, "rus");
-	
-	char str[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-	'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	
-	
-	for (short i = 0; i < strlen(string); i++)
-	{
-		cout << string[i];
-		cout << endl;
-	}
-	
-
-	const short num = 10;
-	const int number = 255;
-	char String[number] = {0};
-	cin.getline(String, number, '0');
-
-
-	//Преременные которые мы будм сравнивать и оставлять 
-	name_Of_Indefekator* name_Of_True = new name_Of_Indefekator[];
-	//string* name_Of_True = new string[num];
-	name_Of_True[num] = {"String", "name_Of_Name", "name", "name_Of_Indefekator", "name_Of_True", "name_Of_Fail"};
-	
-	//Переменные которые мы быдем отсеивать
-	name_Of_Indefekator* name_Of_Fail = new name_Of_Indefekator[];
-	//string* name_Of_Fail = new string[num];
-	name_Of_Fail[num] = { "string", "int", "char", "fail", "true", "bool", "double"};
-
-	cout << "" << strlen(String) << endl;
-	cout << String;
-	
-	return 0;
-}
-
-/**/
-
-
-
-#include <iostream>
-#include <stdlib.h>
 #include <iomanip>
 
 using namespace std;
@@ -63,7 +6,7 @@ using namespace std;
 int main()
 {
     const int n = 7;
-
+    bool a = false;
 
     int matr[n][n];
     int minproizv = 100000;
@@ -91,11 +34,11 @@ int main()
 
     cout << endl << "max = " << max << endl << endl;
 
-    for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++)
     {
         int proizv = 1;
 
-        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
         {
             cout << setw(3) << matr[i][j];
             proizv *= matr[i][j];
@@ -111,12 +54,13 @@ int main()
     for (int i = 0; i < n; i++)
     {
         int sum = 0;
-        bool a = false;
+        
         for (int j = 0; j < n; j++)
         {
             if (matr[i][j] == 6)
             {
                 a = true;
+                break;
             }
         }
         for (int j = 0; j < n; j++)
